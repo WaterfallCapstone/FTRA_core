001f4655396904e99c9a53fa330275b22ed8cdee
LightYe4r <24adrenalin@naver.com>
2023-05-09 01:13:15 +0900

    FIX MotorController: minimum motor value

--- FTRA_core_python/Modules/Motor/MotorController.py
+++ FTRA_core_python/Modules/Motor/MotorController.py
@@ -1,18 +1,27 @@
 import serial
 
 class MotorController:
-    def __init__(self, port, baudrate):
+    def __init__(self, port, baudrate, pastValue = [90, 90, 90, 90, 90, 90]):
         self.py_serial = serial.Serial(
             port = port,
             baudrate = baudrate
         )
+        self.pastValue = pastValue
 
     def arrToString(self, command):
         ret = ' '.join(command)
         return ret
 
+    def checkDiff(self, command):
+        for i in range(6):
+            if abs(self.pastValue[i] - command[i]) < 10:
+                command[i] = self.pastValue[i]
+        return command
+    
     def setMotor(self, command):
-        ret = self.arrToString(command)
+        ret = self.checkDiff(command.tolist())
+        ret = self.arrToString(ret)
+        self.pastValue = ret
         self.py_serial.write(ret.encode())
         
     
